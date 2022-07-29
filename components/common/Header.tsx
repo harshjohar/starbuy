f5e8746baf76cd835fc2c5117407a0e8a2ddb24d
Harsh Johar <hpsj3112@gmail.com>
2022-07-29 17:20:35 +0530

    authentication done

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import React from "react";
 import { MdShoppingCart } from "react-icons/md";
+import { signIn, signOut, useSession } from "next-auth/react";
 function Header() {
+    const { data: session } = useSession();
     return (
         <div className="w-screen  py-2 px-6 flex justify-between items-center font-poppins">
             <ul className="flex-[0.3]  flex justify-start items-center space-x-12 py-2 px-12">
@@ -17,16 +19,26 @@ function Header() {
             </ul>
 
             <div className="flex-[0.4] flex items-center justify-center py-2">
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1200px-Amazon_logo.svg.png"
-                    alt="Starbuy"
-                    className="h-8 object-contain"
-                />
+                <Link href={"/"}>
+                    <img
+                        src="/assets/images/starbuy.png"
+                        alt="Starbuy"
+                        className="h-28 object-contain"
+                    />
+                </Link>
             </div>
 
             <ul className="flex-[0.3]  flex items-center justify-end space-x-12 py-2 px-12">
                 <li className="cursor-pointer hover:underline transition underline-offset-4">
-                    <Link href={"/account"}>Account</Link>
+                    {session ? (
+                        <Link
+                            href={"/account"}
+                        >{`Hello ${session.user?.name}`}</Link>
+                    ) : (
+                        <p className="" onClick={() => signIn()}>
+                            Sign In
+                        </p>
+                    )}
                 </li>
                 <li className="cursor-pointer text-3xl hover:text-starbuy-subtext">
                     <Link href={"/cart"}>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const App = Component;
+    return (
+        <SessionProvider session={pageProps.sessions}>
+            <App {...pageProps} />
+        </SessionProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
