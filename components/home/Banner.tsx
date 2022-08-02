09e9b4389eca7cbfc44e2eda6d24fbfacb3b87da
Harsh Johar <hpsj3112@gmail.com>
2022-08-02 11:30:55 +0530

    Update banners #2 homepage

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import * as LottiePlayer from "@lottiefiles/lottie-player";
 function Banner() {
     return (
         <div className="w-[85%] bg-starbuy-primary h-[600px] mx-auto relative flex">
@@ -16,11 +16,13 @@ function Banner() {
                     <p>Peace</p>
                 </div>
 
-                <button className="bg-black text-white px-6 py-2 text-sm">Shop Now</button>
+                <button className="bg-black text-white px-6 py-2 text-sm">
+                    Shop Now
+                </button>
             </div>
 
             <div className="w-1/2 bg-starbuy-product h-full">
-                <img src="/assets/images/banner.png" alt="" className="h-full w-full object-cover" />
+                <img src="/assets/images/banner.gif" alt="" className="h-full w-full object-cover" />
             </div>
         </div>
     );
